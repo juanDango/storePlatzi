b12beaf5d3a0faffc7a1b65a5e5f8e4fafdb1e38
JuanDango <juandancas@gmail.com>
2023-12-28 16:45:25 -0500

    added carrito de compras

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -9,19 +10,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ProductComponent {
   @Input({required: true})
-  img: string = ''
-
-  @Input({required: true})
-  title: string = ''
-
-  @Input({required: true})
-  price: number = 0
+  product!: Product
 
   @Output()
   addToCart = new EventEmitter();
 
   addToCartHandler(){
-    console.log(`Se espichó desde el hijo ${this.title}`)
-    this.addToCart.emit(`Se ha emitido un evento muajaja ${this.title}`)
+    console.log(`Se espichó desde el hijo ${this.product.title}`)
+    this.addToCart.emit(`Se ha emitido un evento muajaja ${this.product.title}`)
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,14 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([])
+
+  constructor(){
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=12',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'pro 2',
+        price: 200,
+        image: 'https://picsum.photos/640/640?r=15',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'pro 3',
+        price: 300,
+        image: 'https://picsum.photos/640/640?r=200',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'pro 4',
+        price: 200,
+        image: 'https://picsum.photos/640/640?r=102',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'pro 5',
+        price: 2100,
+        image: 'https://picsum.photos/640/640?r=70',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'pro 6',
+        price: 300,
+        image: 'https://picsum.photos/640/640?r=2000',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'pro 7',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=43',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'pro 8',
+        price: 2050,
+        image: 'https://picsum.photos/640/640?r=185',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'pro 9',
+        price: 30000,
+        image: 'https://picsum.photos/640/640?r=2080',
+        createdAt: new Date().toISOString()
+      }
+    ]
+
+    this.products.set(initProducts)
+  }
+
   fromChild(event: string){
     console.log('Esto es lo que hace el padre')
     console.log(event)
