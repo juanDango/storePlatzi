fce14d4abe2130a0ea22c0a0e75dbaf9268f7892
JuanDango <juandancas@gmail.com>
2023-12-29 13:59:51 -0500

    Filter by categories

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,25 +1,49 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, HeaderComponent],
+  imports: [ProductComponent, HeaderComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
 
   products = signal<Product[]>([])
+  categories = signal<Category[]>([])
   private cartService = inject(CartService)
   private productSevice = inject(ProductService)
+  private categoryService = inject(CategoryService)
+
+  @Input()
+  category_id?: string;
 
   ngOnInit(){
-    this.productSevice.getProducts()
+    this.getProducts()
+    this.getCategories()
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    const category_id = changes['category_id']
+    if(category_id){
+      this.getProducts(category_id.currentValue)
+    }
+  }
+
+  addToCart(event: Product){
+    this.cartService.addToCart(event)
+  }
+
+  private getProducts(category_id?: string) {
+    this.productSevice.getProducts(category_id)
     .subscribe({
       next: (products) => {
         this.products.set(products)
@@ -27,7 +51,12 @@ export class ListComponent {
     })
   }
 
-  addToCart(event: Product){
-    this.cartService.addToCart(event)
+  getCategories(){
+    this.categoryService.getCategories()
+    .subscribe({
+      next: (categories) => {
+        this.categories.set(categories)
+      }
+    })
   }
 }
--- src/app/domains/shared/services/category.service.ts
+++ src/app/domains/shared/services/category.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Category } from '@shared/models/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
+  private http = inject(HttpClient)
+
   constructor() { }
+
+  getCategories(){
+    return this.http.get<Category[]>('https://api.escuelajs.co/api/v1/categories')
+  }
 }
