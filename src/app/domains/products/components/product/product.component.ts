3ac5b29932a537c12f2b98b9d9023050bb290a3d
JuanDango <juandancas@gmail.com>
2023-12-29 12:19:49 -0500

    pipes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,11 +1,19 @@
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
 import { Product } from '@shared/models/product.model';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [UpperCasePipe, CurrencyPipe, DatePipe],
+  imports: [
+    UpperCasePipe,
+    CurrencyPipe,
+    DatePipe,
+    ReversePipe,
+    TimeAgoPipe
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -16,7 +24,10 @@ export class ProductComponent {
   @Output()
   addToCart = new EventEmitter();
 
+  fecha = signal<string>((new Date()).toISOString())
+
   addToCartHandler(){
+    console.log(this.fecha())
     console.log(`Se espichó desde el hijo ${this.product.title}`)
     this.addToCart.emit(this.product)
   }
